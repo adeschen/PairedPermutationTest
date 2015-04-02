e7a61d451b82ce3ef5eb5fea249e2fffefe02751
adeschen <adeschen@hotmail.com>
2015-04-02 13:55:27 -0400

    Adding roxyegen doc for package

---
+++ R/PairedPermutationTest.R
@@ -0,0 +1,18 @@
+#' PairedPermutationTest: Permutation tests on paired observations
+#'
+#' \tabular{ll}{ 
+#' Package: \tab PairedPermutationTest\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.99.0\cr 
+#' Date: \tab 2014-10-15\cr 
+#' License: \tab Artistic-2.0\cr 
+#' }
+#'
+#' Maintainer: Astrid Louise Deschenes <Astrid-Louise.Deschenes@@crchudequebec.ulaval.ca> 
+#' 
+#' @docType package
+#' @name PairedPermutationTest-package
+#' @aliases PairedPermutationTest-package PairedPermutationTest
+#' @author Astrid Louise Deschenes
+#' @keywords package
+NULL
